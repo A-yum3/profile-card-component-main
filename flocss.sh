459df2369fa22ce2954e152abc902510f8658506
javateaboy <noikari73@gmail.com>
2021-04-25 17:33:57 +0900

    complate

---
+++ flocss.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+mkdir scss;
+cd scss;
+mkdir foundation layout object;
+cd object;
+mkdir component project utils js;
+cd ../;
+touch style.scss;
+touch foundation/_reset.scss;
+touch foundation/_base.scss;
